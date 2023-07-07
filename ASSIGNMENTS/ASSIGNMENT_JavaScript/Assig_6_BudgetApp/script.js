852ec46fef080ba852fb0455f176f4cd89fdbeae
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-07-07 15:16:10 +0530

    js-budget-app

---
+++ ASSIGNMENTS/ASSIGNMENT_JavaScript/Assig_6_BudgetApp/script.js
@@ -0,0 +1,6 @@
+function displayNumber() {
+      var inputNumber = document.getElementById("numberInput").value;
+      var tableData = document.getElementById("numberCell");
+      
+      tableData.innerText = inputNumber;
+    }
